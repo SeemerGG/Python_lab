3f83e3f54a7d719c97435b72c863a7cf35142ecd
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-10-21 12:33:59 +0300

    2

--- ID_num_3_classes/ID_num_3_classes/ID_num_3_classes.py
+++ ID_num_3_classes/ID_num_3_classes/ID_num_3_classes.py
@@ -1,3 +1,6 @@
+from cmath import e
+
+
 class polygon(object):
 
     def __init__(self, xy, name):
@@ -12,36 +15,75 @@ class polygon(object):
 
     def is_intersect(self, obj2):
         vertexes = [0, 0, 0, 0]
-        flag = False
         for i in self.edges:
             for j in obj2.edges:
-                #vertexes.append((j[1][0] - j[0][0]) * (i[0][1] - i[0][0]) - (j[1][1] - j[0][1]) * (i[0][0] - j[0][0]))
-                #vertexes.append((j[1][0] - j[0][0]) * (i[1][1] - i[0][0]) - (j[1][1] - j[0][1]) * (i[1][0] - j[0][0]))
-                #vertexes.append((i[1][0] - i[0][0]) * (j[0][1] - i[0][1]) - (i[1][1] - i[0][1]) * (j[0][0] - i[0][0]))
-                #vertexes.append((i[1][0] - i[0][0]) * (j[1][1] - i[0][1]) - (i[1][1] - i[0][1]) * (j[1][0] - i[0][0]))
                 vertexes[0] = ((j[1][0] - j[0][0]) * (i[0][1] - i[0][0]) - (j[1][1] - j[0][1]) * (i[0][0] - j[0][0]))
                 vertexes[1] = ((j[1][0] - j[0][0]) * (i[1][1] - i[0][0]) - (j[1][1] - j[0][1]) * (i[1][0] - j[0][0]))
                 vertexes[2] = ((i[1][0] - i[0][0]) * (j[0][1] - i[0][1]) - (i[1][1] - i[0][1]) * (j[0][0] - i[0][0]))
                 vertexes[3] = ((i[1][0] - i[0][0]) * (j[1][1] - i[0][1]) - (i[1][1] - i[0][1]) * (j[1][0] - i[0][0]))
-                flag = (vertexes[0] * vertexes[1] < 0) and (vertexes[2] * vertexes[3] < 0)
-                if flag: 
-                    return flag
-        return flag
+                if (vertexes[0] * vertexes[1] < 0) and (vertexes[2] * vertexes[3] < 0):
+                    return True
+        return False
     
     def is_include(self, obj2):
+        if self.is_intersect(obj2):
+            proj2_x = abs(max(obj2.xy)[0] - min(obj2.xy)[0])
+            proj1_x = abs(max(self.xy[0]) - min(self.xy)[0])
+            proj2_y = abs(max(obj2.xy, key = lambda i : i[1])[1] - min(obj2.xy,key =  lambda i:i[1])[1])
+            proj1_y = abs(max(self.xy, key = lambda i : i[1])[1] - min(self.xy,key =  lambda i:i[1])[1])
+            if proj1_x - proj2_x > 0 and proj1_y - proj2_y > 0:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 
+class triangle(polygon):
 
+    def __init__(self, xy, name):
+
+        try:
+            self.name = name
+            self.xy = xy
+            self.edges = []
+            for i in range(len(xy)):
+                if i < len(xy) - 1:
+                    self.edges.append([xy[i],xy[i+1]])
+                else:
+                    self.edges.append([xy[i],xy[0]])
+        except:
+            print("Error")
+
+
+class rectangle(polygon):
+
+    def __init__(self, xy, name):
+
+        try:
+            self.name = name
+            self.xy = xy
+            self.edges = []
+            for i in range(len(xy)):
+                if i < len(xy) - 1:
+                    self.edges.append([xy[i],xy[i+1]])
+                else:
+                    self.edges.append([xy[i],xy[0]])
+        except:
+            print("Error")            
+
+        
         
 
 
 
 
+
 def main():
-    trin1 = polygon([[1,2], [3,4], [4,2]], "pol1")
-    trin2 = polygon([[0,0], [1,1], [2,0]], "pol2")
+    trin1 = triangle([[1,3], [2,5], [3,3]], "pol1")
+    trin2 = rectangle([[0,0], [0,5], [5,5], [5,0]], "pol2")
     
-    print(trin1.is_intersect(trin2))
+    print(trin1.is_include(trin2))
 
 main()
 
