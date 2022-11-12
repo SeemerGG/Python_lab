c4e851f134de28be361c9aca16ec1e7a75add808
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-11-12 16:13:03 +0300

    1

--- IT_BD/IT_BD/IT_BD.py
+++ IT_BD/IT_BD/IT_BD.py
@@ -3,7 +3,9 @@ from sqlite3 import *
 def create_database():
     with connect('architecture.db') as db:
         cursor =  db.cursor()
-        querys = [""" create table property_developer(name_company text not null primary key, adress_main_office text, hotline_phone_number text)""", """ create table building(id_building integer not null primary key, adress_building text, type_building text, name_company text not null, foreign key (name_company) references property_developer(name_company))""", """ create table builder(id_builder integer not null primary key, full_name text, salary integer,id_building integer not null, foreign key (id_building) references building(id_building))"""]
+        querys = [""" create table property_developer(name_company text not null primary key, adress_main_office text, hotline_phone_number text)""",
+		""" create table building(id_building integer primary key autoincrement not null, adress_building text, type_building text, name_company text not null, foreign key (name_company) references property_developer(name_company))""", 
+		""" create table builder(id_builder integer primary key autoincrement, full_name text, salary integer,id_building integer not null, foreign key (id_building) references building(id_building))"""]
         for i in querys:
             cursor.execute(i)
 
@@ -11,9 +13,17 @@ def create_database():
 def input_data():
     with connect('architecture.db') as db: 
         cursor = db.cursor()
-        data = [""" insert into property_developer values('SSK', 'Krasnodar, Fadeeva street, 214', '88002223550')""", """ insert into property_developer values('NVM', 'Krasnodar, 2th-Yamalskaya street, 1', '89612121741')""",""" insert into property_developer values('DOSTOYANIE', 'Rostov-on-Don, Filimonovskaya, 45', '88632506007')""", """ insert into building values(12, 'Stasovo street, 213', 'residential building', 'DOSTOYANIE')""", """insert into building values(13,'Stavropolskaya street, 34', 'residential building', 'SSK')""", """insert into building values(14, 'Old-Kubanskaya street, 123', 'residential building', 'NVM')""", """ insert into builder values(456,'Ivanov Ivan Ivanovich', 15000, 12)""", """insert into builder values(457, 'Volodeev Aleksandr Alekseevich', 16000, 13)""", """insert into builder values(458, 'Dimitrov Dmitriy Dmitrivich', 36000, 14)"""]
+        data = [""" insert into property_developer values('SSK', 'Krasnodar, Fadeeva street, 214', '88002223550')""",
+		""" insert into property_developer values('NVM', 'Krasnodar, 2th-Yamalskaya street, 1', '89612121741')""",
+		""" insert into property_developer values('DOSTOYANIE', 'Rostov-on-Don, Filimonovskaya, 45', '88632506007')""",
+		""" insert into building(adress_building, type_building, name_company) values('Stasovo street, 213', 'residential building', 'DOSTOYANIE')""",
+		"""insert into building(adress_building, type_building, name_company) values('Stavropolskaya street, 34', 'residential building', 'SSK')""", 
+		"""insert into building(adress_building, type_building, name_company) values('Old-Kubanskaya street, 123', 'residential building', 'NVM')""",
+        """insert into builder(full_name, salary, id_building) values('Ivanov Ivan Ivanovich', 36000, 1)""",
+        """insert into builder(full_name, salary, id_building) values('Sirgay Antonovich Driche', 36000, 2)""",
+        """insert into builder(full_name, salary, id_building) values('Ivanov Ivan Daichev', 30000, 3)"""]
         for i in data:
-	        cursor.execute(i)
+	        cursor.execute(i)   
 
 
 
@@ -26,9 +36,8 @@ def get_output_data_about_builder():
             print(cursor.fetchall())
 
 
-def main():
-    create_database()
-    input_data()
-    get_output_data_about_builder()
+
+create_database()
+input_data()
 
 
---
+++ IT_BD/IT_BD/cgi-bin/add_node_builder.py
@@ -0,0 +1,35 @@
+﻿#!/usr/bin/env python3
+from sqlite3 import *
+import cgi
+
+def add_note_builder(full_name, salary_bulder, id_bulding):
+	with connect('architecture.db') as db:
+		cursor  = db.cursor()
+		data_templayt = """insert into builder(full_name, salary, id_building) values('{name}', {salary}, {id_bulding})"""
+		data = data_templayt.format(name = full_name, salary = salary_bulder, id_bulding = id_bulding)
+		cursor.execute(data)
+
+def main():
+	form = cgi.FieldStorage()
+	full_name = form.getfirst("full_name", "Инакентий")
+	salary = form.getfirst("salary", 0)
+	id_building = form.getfirst("id_building", 0)
+
+	add_note_builder(full_name, salary, id_building)
+		
+	print("Content-type: text/html\n")
+	print("""<!DOCTYPE HTML>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Добавление записи</title>
+	</head>
+	<body>
+		<form action="/Padges/add_node_padges.html">
+			<p>Данные внесены</p>
+			<button type="submit" >Вернутся назад</button>
+		</form>
+	</body>
+	</html>""")
+
+main()
